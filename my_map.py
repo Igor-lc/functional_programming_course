81df0af7fe32bdb3cc2b9615cf40c111371ed353
Igor-lc <105611667+Igor-lc@users.noreply.github.com>
2023-05-08 00:46:26 +0300

    Add files via upload

--- my_map.py
+++ my_map.py
@@ -1,13 +1,13 @@
-numbers1 = [1, 2, 3]
-numbers2 = [4, 5]
+numbers1 = [1, 2, 3, 2]
+numbers2 = [4, 5, 6]
 
 
 def mymap(funk, *args):
     res = []
-    for i in range(len(min(args, key=len))):
-        print(args[i][i])
-        res.append(funk[args[i][i]] + funk[args][i][i])
-        return res
+    for i in zip(*args):
+        res.append(funk(*i))
+    return res
 
-result = mymap(lambda x, y: x + y, numbers1, numbers2)
-print(list(result))
+
+
+print(list(mymap(lambda x, y: x + y, numbers1, numbers2)))
