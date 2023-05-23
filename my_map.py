4882df26f5830f69316fee1063f6dd41f11c28b8
Igor-lc <105611667+Igor-lc@users.noreply.github.com>
2023-05-23 12:40:42 +0300

    Add files via upload

--- my_map.py
+++ my_map.py
@@ -2,14 +2,34 @@ numbers1 = [1, 2, 3, 2]
 numbers2 = [" python", "SQL", " PHP "]
 
 
-def mymap(funk, *args):
-    res = []
+def mymap(func, *seq):
+    newseq = []
+
+    # Ищем размер минимальной последовательности
+    min_list_len = min([len(s) for s in seq])
+
+    # Теперь идем по элементам последовательностей
+    for i in range(min_list_len):
+        # Формируем аргументы функции из элементов последовательностей
+        func_args = []
+        for s in seq:
+            func_args.append(s[i])
+
+        # Раскрываем fun_args и вставляем в функцию
+        newseq.append(func(*func_args))
+
+    return newseq
+
+
+def mymap2(funk, *args):
+    newseq = []
     for i in zip(*args):
-        res.append(funk(*i))
-    return res
+        newseq.append(funk(*i))
+    return newseq
 
 
 print(list(mymap(str.lower, numbers2)))
+print(list(mymap2(str.lower, numbers2)))
 
 
 
